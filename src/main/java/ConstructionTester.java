f82f1b8dc8cab819a0bd5c2a62e490daea2dafb5
Brendan Sirls <brendan.142408@nv.ccsd.net>
2020-09-26 19:11:22 +0000

    Construction

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,24 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        Scanner input = new Scanner(System.in);
+        double lumbCost = 8;
+        double winCost = 11;
+        double tax, total;
+        int numBoards, numWindows;
+        
+       System.out.println("Enter the sales tax rate: ");
+       tax = input.nextDouble();
+       System.out.println("How many boards do you need? ");
+       numBoards = input.nextInt();
+       System.out.println(" How many windows do you need? ");
+       numWindows = input.nextInt();
+
+       Construction test = new Construction(lumbCost, winCost, tax); 
+       
+       total = test.lumberCost(numBoards) + test.windowCost(numWindows);
+       System.out.println("Total: " + total);
+      
+       System.out.println("Grand Total: " + test.grandTotal(total));
     }
 }
